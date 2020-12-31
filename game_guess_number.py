7b0c18d6bc58ccc1e115915ba804285d08592619
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2020-12-31 23:15:22 +0330

    add game guess number

--- game_guess_number.py
+++ game_guess_number.py
@@ -1,3 +1,12 @@
 
 import random
-print(random.randint(0, 10))
+current_number = random.randint(0, 10)
+print(current_number)
+
+while True:
+    guess = int(input("Guess a number between 0 and 10:"))
+    if guess == current_number:
+        print("wine!")
+        break
+    else:
+        print("lost!")
