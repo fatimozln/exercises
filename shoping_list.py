a455a36372b355f089d7998f1e0b7a95993f9ee4
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2020-12-31 22:37:14 +0330

    update shoping_list

--- shoping_list.py
+++ shoping_list.py
@@ -1,11 +1,33 @@
 
 shoping_list = []
 
+print("please enter your requirements =")
+
+
+def show_help():
+    print("""
+    enter <done> to stop add list
+    enter <show> to shoe your list
+    """)
+
+
+def show_list():
+    print("your list:", shoping_list)
+
+
 while True:
     item = input("enter your item:")
-    if not item == "done":
-        shoping_list.append(item)
-    else:
+
+    if item == "done":
         break
 
-print("your list:", shoping_list)
+    elif item == "show":
+        show_list()
+
+    elif item == "help":
+        show_help()
+
+    elif not item in shoping_list:
+        shoping_list.append(item)
+
+show_list()
