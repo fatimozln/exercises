bdb034521c66671419f003205809af64309acd76
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-01-04 13:56:58 +0330

    update Limit the number of guesses

--- game_guess_number.py
+++ game_guess_number.py
@@ -1,12 +1,27 @@
 
 import random
-current_number = random.randint(0, 10)
+current_number = random.randint(1, 10)
 print(current_number)
 
-while True:
-    guess = int(input("Guess a number between 0 and 10:"))
-    if guess == current_number:
-        print("wine!")
-        break
+
+def run_game():
+    a = 0
+    while a < 5:
+        try:
+            guess = int(input("Guess a number between 1 and 10:"))
+        except ValueError:
+            print("your inpute not number")
+        else:
+            if guess == current_number:
+                print("wine!the current number is", current_number)
+                break
+            else:
+                print("lost!")
+
+            a += 1
+
     else:
-        print("lost!")
+        print("You could not guess correctly. Correct number =", current_number)
+
+
+run_game()
